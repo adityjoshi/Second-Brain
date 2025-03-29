5ec2b5cc2a6f8c328060dbfb7a6d5088bfea313d
Aditya Joshi <111140014+adityjoshi@users.noreply.github.com>
2025-03-29 14:19:26 +0530

    tcp

--- tcp/main.go
+++ tcp/main.go
@@ -1,21 +1,52 @@
 package main
 
 import (
-	"fmt"
 	"log"
 	"net"
 )
 
 func main() {
-
 	listener, err := net.Listen("tcp", ":1729")
 	if err != nil {
-		log.Fatal("error :", err)
+		log.Fatal("Error starting server:", err)
 	}
+	defer listener.Close()
 
-	conn, err := listener.Accept()
-	if err != nil {
-		log.Fatal(err)
+	log.Println("Server is listening on port 8080")
+
+	for {
+		conn, err := listener.Accept()
+		if err != nil {
+			log.Println("Error accepting connection:", err)
+			continue
+		}
+
+		log.Println("Client connected from:", conn.RemoteAddr())
+
+		go handleClient(conn)
 	}
-	fmt.Print(conn)
+}
+
+func handleClient(conn net.Conn) {
+	defer conn.Close()
+	buffer := make([]byte, 1024)
+
+	for {
+		n, err := conn.Read(buffer)
+		if err != nil {
+			log.Println("Client disconnected:", conn.RemoteAddr())
+			return
+		}
+
+		receivedData := string(buffer[:n])
+		log.Printf("Received from client (%s): %s\n", conn.RemoteAddr(), receivedData)
+
+		response := "Hello from the server!\n"
+		_, err = conn.Write([]byte(response))
+		if err != nil {
+			log.Println("Error sending response:", err)
+			return
+		}
+		log.Println("Response sent to client.")
+	}
 }

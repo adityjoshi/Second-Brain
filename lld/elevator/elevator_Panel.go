49f1dfe92acc14b911bc79763df2050425447f61
adi <aditya03joshi@gmail.com>
2026-06-04 14:07:17 +0530

    add remove floor button

--- lld/elevator/elevator_Panel.go
+++ lld/elevator/elevator_Panel.go
@@ -8,3 +8,11 @@ type ElevatorPanel struct {
 func NewElevatorPanel(panelID int) *ElevatorPanel {
 	return &ElevatorPanel{PanelID: panelID, FloorButtons: [15]bool{}}
 }
+
+func (ep *ElevatorPanel) AddDestinationFloor(floor int) {
+	ep.FloorButtons[floor] = true
+}
+
+func (ep *ElevatorPanel) RemoveDestination(floor int) {
+	ep.FloorButtons[floor] = false
+}
